8c82498f67beba56baf314b0e984d105597fe657
Richard Fröhning <misanthropos@gmx.de>
2024-07-14 07:45:18 +0200

    Make some more tests before downloading.
    Signed-off-by: Richard Fröhning <misanthropos@gmx.de>

--- get-gluon-archive.sh
+++ get-gluon-archive.sh
@@ -12,15 +12,20 @@ MAXRETRIES=3
 ARCHSITE="http://freifunk.madhacker.biz:6666/gluon"
 SHAFILE=${GLUONARCHIVE}.sha256sum
 cd "${WORKDIR}"
-wget "${ARCHSITE}/${SHAFILE}" -O ${SHAFILE}
 
-ARCHFILE=$(cut -d " " -f 3 ${SHAFILE})
-
-if [ ! -f "${ARCHFILE}" ]; then
-    wget -c "${ARCHSITE}/${ARCHFILE}"
+if  ! (diff -N -q <(wget -q -O -  "${ARCHSITE}/${SHAFILE}")  "${SHAFILE}") ; then
+    wget "${ARCHSITE}/${SHAFILE}" -O ${SHAFILE}
+else
+    echo "No new Archive! Testing local archive."
+    if (sha256sum -c ${SHAFILE}); then
+        exit
+    fi
 fi
+    
+ARCHFILE=$(cut -d " " -f 3 ${SHAFILE})
+wget -c "${ARCHSITE}/${ARCHFILE}"
 
-## get and checksum archive
+## checksum archive and retrie if check fails
 idx=0
 until sha256sum --status -c ${SHAFILE} ; do
     wget -c "${ARCHSITE}/${ARCHFILE}"
