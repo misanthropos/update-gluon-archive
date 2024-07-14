5cca7aef53300ae041829d95030ed20d4b40b9b3
Richard Fröhning <misanthropos@gmx.de>
2024-07-14 05:37:04 +0200

    Retrieve and checksum a complete gluon archive.
    Signed-off-by: Richard Fröhning <misanthropos@gmx.de>

---
+++ get-gluon-archive.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    GLUONARCHIVE=hellfire
+    WORKDIR="$(dirname "${BASH_SOURCE[0]}")"
+else
+    GLUONARCHIVE="${1}"
+    WORKDIR="${2}"
+fi
+
+MAXRETRIES=3
+ARCHSITE="http://freifunk.madhacker.biz:6666/gluon"
+SHAFILE=${GLUONARCHIVE}.sha256sum
+cd "${WORKDIR}"
+wget "${ARCHSITE}/${SHAFILE}" -O ${SHAFILE}
+
+ARCHFILE=$(cut -d " " -f 3 ${SHAFILE})
+
+if [ ! -f "${ARCHFILE}" ]; then
+    wget -c "${ARCHSITE}/${ARCHFILE}"
+fi
+
+## get and checksum archive
+idx=0
+until sha256sum --status -c ${SHAFILE} ; do
+    wget -c "${ARCHSITE}/${ARCHFILE}"
+    ((idx++))
+    if [ $idx == 3 ]; then
+        echo "Failed to download archive ${ARCHFILE}" >&2
+        exit 1
+    fi
+done
+
+
+
+     
